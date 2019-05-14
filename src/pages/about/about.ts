cffcd9a5eec597751c26a7f4e273975c00d6c0a7
Victor Andrade <victor.ferreira@nanoincub.com.br>
2019-05-13 21:17:32 -0300

    Primeiro commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import * as firebase from 'firebase'
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+    ref_pedidos = firebase.database().ref('pedido/');
 
-  }
+    constructor(public navCtrl: NavController) {
+    }
+
+    ngOnInit() {
+        this.ref_pedidos.on('value', async res => {
+            await res.forEach(data => {
+                console.log(data.val());
+            });
+        });
+    }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import * as firebase from 'firebase';
+import {datasnapshotToArray} from "../../app/envroiment";
+import {AddLanchesPage} from "../add-lanches/add-lanches";
+import {DetalhesLanchesPage} from "../detalhes-lanches/detalhes-lanches";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    items = [];
+    rfence = firebase.database().ref('lanches/');
 
-  }
+    constructor(public navCtrl: NavController) {
 
+    }
+
+    ngOnInit() {
+
+        this.initialize();
+
+    }
+
+    async initialize() {
+        await this.rfence.on('value', async res => {
+            this.items = [];
+            await res.forEach(data => {
+                this.items.push(data.val());
+            });
+        });
+
+        await console.log(this.items);
+    }
+
+    addLanches() {
+
+        this.navCtrl.push(AddLanchesPage);
+
+    }
+
+    detalhesLanche() {
+        this.navCtrl.push(DetalhesLanchesPage);
+    }
 }
